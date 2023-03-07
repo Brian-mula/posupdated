ba04678febfa1d370448d5476cf4138b750c6762
Brian-mula <mulatiwangilabrian@gmail.com>
2023-03-07 01:10:49 -0800

    added the get pacckage for size management

--- lib/views/products/products.dart
+++ lib/views/products/products.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:posupdated/views/utils/dimensions.dart';
 import 'package:posupdated/widgets/custome_input.dart';
 
 class Products extends ConsumerStatefulWidget {
@@ -14,8 +15,11 @@ class _ProductsState extends ConsumerState<Products> {
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    print(MediaQuery.of(context).size.height.toString());
     return Scaffold(
       body: Container(
+        height: double.maxFinite,
         padding: const EdgeInsets.only(left: 10, right: 10),
         child: Column(
           children: [
@@ -25,7 +29,41 @@ class _ProductsState extends ConsumerState<Products> {
                 controller: controller,
                 label: "Search in products....",
               ),
-            )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Most popular products",
+              style: theme.textTheme.headlineSmall!
+                  .copyWith(color: Colors.orange.shade600),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+                height: Dimensions.containerheight450,
+                child: GridView.builder(
+                    itemCount: 10,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      crossAxisCount: 2,
+                    ),
+                    itemBuilder: (context, index) => Container(
+                        padding: const EdgeInsets.only(left: 2, right: 2),
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 150,
+                                height: 100,
+                                color: Colors.blue,
+                              )
+                            ],
+                          ),
+                        )))),
           ],
         ),
       ),
---
+++ lib/views/utils/dimensions.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screenheight = Get.context!.height;
+  static double screenwidth = Get.context!.width;
+
+  // ! container height for the gridview
+  static double containerheight450 = screenheight / 1.38;
+}
